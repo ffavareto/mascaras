a1122d1d9ffe2653bf7bb78f10fdc92ef0ab8526
Lucas Santos <lucas.santos@faculdadeiv2.com.br>
2020-08-19 16:49:44 -0300

    melhorando mascara cpf

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const cpf = document.getElementById("cpf")
 
-function formataCPF() {
+
+
+cpf.addEventListener('input', function () {
   if (cpf.value.length == 3) this.value += "."
   if (cpf.value.length == 7) this.value += "."
   if (cpf.value.length == 11) this.value += "-"
-}
+})
 
-cpf.addEventListener('input', formataCPF)
